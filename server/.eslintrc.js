a0f3805effe9731f9ae87378800d68b72462120f
wdjcodes <wdjcodes@users.noreply.github.com>
2019-02-22 15:29:04 -0500

    Initialized server backend

---
+++ server/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "airbnb-base",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly",
+        describe: true,
+        it: true,
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
